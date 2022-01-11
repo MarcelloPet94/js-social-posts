077ec968e103439f470648a95487e9a02010293a
marcello <marcellopettenuzzo2@gmail.com>
2022-01-11 17:57:21 +0100

    like

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 /*
-Milestone 1
-Creiamo il nostro array di oggetti che rappresentano ciascun post.
-Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
-id del post, numero progressivo da 1 a n
-nome autore,
-foto autore,
-data in formato americano (mm-gg-yyyy),
-testo del post,
-immagine (non tutti i post devono avere una immagine),
-numero di likes.
-Non è necessario creare date casuali
-Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
+
 */
 const posts = [
     {
@@ -94,45 +86,54 @@ const posts = [
 const container = document.getElementById("container");
 
 // faccio uscire i dati nella pagina
-for(let i = 0; i <= posts.length; i++)
+for (let i = 0; i < posts.length; i++)
 {
 
-// date type
-let date1 = posts[i].created.year + '-' + posts[i].created.month + '-' + posts[i].created.day;
-let date2 = posts[i].created.month + '-' + posts[i].created.day + '-' + posts[i].created.year;
+    // scegli tipo di data
+    let date2 = posts[i].created.month + '-' + posts[i].created.day + '-' + posts[i].created.year;
 
-container.innerHTML +=
-`
-<div class="post ${posts[i].id}">
-    <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="${posts[i].author.image}" alt="">                    
+    container.innerHTML +=
+    `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${posts[i].author.image}" alt="">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${posts[i].author.name}</div>
+                    <div class="post-meta__time">${date2}</div>
+                </div>                    
             </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">${posts[i].author.name}</div>
-                <div class="post-meta__time">${date2}</div>
-            </div>                    
         </div>
+        <div class="post__text">${posts[i].content}</div>
+        <div class="post__image">
+            <img src="${posts[i].media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div id="btn-${posts[i].id}" class="likes__cta">
+                    <a class="like-button  js-like-button" href="#!" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${posts[i].id}" class="js-likes-counter">${posts[i].likes}</b> persone
+                </div>
+            </div> 
+        </div>            
     </div>
-    <div class="post__text">${posts[i].content}</div>
-    <div class="post__image">
-        <img src="${posts[i].media}" alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
-            </div>
-        </div> 
-    </div>            
-</div>
-`
+    `
 } 
 
+let likeButton = document.getElementsByClassName('like-button');
+let jsLikesCounter = document.getElementsByClassName('js-likes-counter');
+
+for (let c = 0; c < posts.length; c++)
+{
+    likeButton[c].addEventListener('click' , function()
+    {
+        jsLikesCounter[c].innerHTML = posts[c].likes + 1;
+    });
+}
